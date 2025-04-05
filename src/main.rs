4b8b856630cda0011c147f5c1c0220bd172061dc
rickypang0219 <rickypang0219@gmail.com>
2025-04-05 23:36:23 +0800

    add intro to readme

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,21 @@ fn main() {
         .spawn()
         .expect("Failed to start bookticker listener");
 
-    // let mut order_update_process = Command::new("cargo")
-    //     .arg("run")
-    //     .arg("--release")
-    //     .arg("--bin")
-    //     .arg("order_update")
-    //     .stdout(Stdio::inherit()) // Use the same stdout as the parent
-    //     .spawn()
-    //     .expect("Failed to start order update listener");
+    let mut order_update_process = Command::new("cargo")
+        .arg("run")
+        .arg("--release")
+        .arg("--bin")
+        .arg("order_update")
+        .stdout(Stdio::inherit()) // Use the same stdout as the parent
+        .spawn()
+        .expect("Failed to start order update listener");
 
     let _ = bookticker_process
         .wait()
         .expect("Bookticker listener failed");
-    // let _ = order_update_process
-    //     .wait()
-    //     .expect("Order update listener failed");
+    let _ = order_update_process
+        .wait()
+        .expect("Order update listener failed");
 
     println!("Both listeners have finished.");
 }
